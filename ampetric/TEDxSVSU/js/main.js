eb940861ffe208f8e5fde0554b966578c0bfefc3
Andrew M. Petricevic <ampetric@csis.svsu.edu>
2016-11-16 13:20:03 -0500

    ampetric: edited course_planner

--- ampetric/TEDxSVSU/js/main.js
+++ ampetric/TEDxSVSU/js/main.js
@@ -2,7 +2,7 @@ jQuery(function($) {'use strict',
 	
 	//Countdown js
 	 $("#countdown").countdown({
-			date: "10 july 2017 12:00:00",
+			date: "30 september 2017 12:00:00",
 			format: "on"
 		},
 		
@@ -75,10 +75,10 @@ jQuery(function($) {'use strict',
 
 	map = new GMaps({
 		el: '#gmap',
-		lat: 43.04446,
-		lng: -76.130791,
+		lat: 43.5126,
+		lng: -83.9605,
 		scrollwheel:false,
-		zoom: 16,
+		zoom: 15,
 		zoomControl : false,
 		panControl : false,
 		streetViewControl : false,
@@ -89,8 +89,8 @@ jQuery(function($) {'use strict',
 
 	var image = 'images/map-icon.png';
 	map.addMarker({
-		lat: 43.04446,
-		lng: -76.130791,
+		lat: 43.513,
+		lng: -83.965,
 		icon: image,
 		animation: google.maps.Animation.DROP,
 		verticalAlign: 'bottom',
